2ba3aeba3edef6f0e4818f1350756ce293abb2c8
Vitaliy <vetal9305@gmail.com>
2025-11-10 12:40:49 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "./",
   build: {
-    outDir: "public", 
+    outDir: "public",
   },
 });
